2d2b4bec309e0c2f66d6a7b23f7c97a73ec85e7d
Justus Bogner <justus.bogner@hpe.com>
2018-04-04 16:04:04 +0200

    Templatized persona view draft

--- src/components/personas/Generator.ts
+++ src/components/personas/Generator.ts
@@ -10,14 +10,144 @@ export class Generator {
                 name: "Mustermann",
                 country: "Germany",
                 age: 35,
-                gender: "male"
+                gender: "male",
+                street: "Main Street",
+                number: "12",
+                zipCode: 75432,
+                city: "Neustadt",
+                image: "",
+                education: "Diplom Informatiker",
+                quote: "'Without requirements or design, programming is the art of adding bugs to an empty text file.' - Louis Srygley",
+                languages: [""],
+                currentJob: {
+                    jobTitle: "Junior Java Developer",
+                    company: "",
+                    mostUsedProgrammingLanguage: "",
+                    durationInMonths: 17,
+                    numberOfEmployees: 200000
+                },
+                previousJobs: [
+                    {
+                        jobTitle: "Test Engineer",
+                        company: "Microsoft",
+                        mostUsedProgrammingLanguage: "Java",
+                        durationInMonths: 17,
+                        numberOfEmployees: 200000
+                    },
+                    {
+                        jobTitle: "Developer",
+                        company: "Siemens",
+                        mostUsedProgrammingLanguage: "C",
+                        durationInMonths: 24,
+                        numberOfEmployees: 300000
+                    }
+                ],
+                favoriteColor: "Red",
+                favoriteOperatingSystem: "Windows 10",
+                hobbies: [""],
+                keyAttributes: ["9-5 job", "Features, Features, Features"],
+                personalDrive: [""],
+                preferredCommunicationChannels: ["Slack", "IRC", "Twitter"],
+                programmingExperiences: [
+                    {
+                        language: "Java",
+                        experienceInYears: 4
+                    },
+                    {
+                        language: "C",
+                        experienceInYears: 7
+                    },
+                    {
+                        language: "PHP",
+                        experienceInYears: 3
+                    }
+                ],
+                usedTechnologies: [
+                    {
+                        name: "Git",
+                        experienceLevel: 3
+                    },
+                    {
+                        name: "Jenkins",
+                        experienceLevel: 2
+                    },
+                    {
+                        name: "Maven",
+                        experienceLevel: 4
+                    }
+                ]
             },
             {
                 prename: "Erika",
                 name: "Mustermann",
                 country: "Germany",
                 age: 35,
-                gender: "female"
+                gender: "female",
+                street: "Main Street",
+                number: "12",
+                zipCode: 75432,
+                city: "Neustadt",
+                image: "",
+                education: "Diplom Informatiker",
+                quote: "'Without requirements or design, programming is the art of adding bugs to an empty text file.' - Louis Srygley",
+                languages: [""],
+                currentJob: {
+                    jobTitle: "Junior Java Developer",
+                    company: "",
+                    mostUsedProgrammingLanguage: "",
+                    durationInMonths: 17,
+                    numberOfEmployees: 200000
+                },
+                previousJobs: [
+                    {
+                        jobTitle: "Test Engineer",
+                        company: "Microsoft",
+                        mostUsedProgrammingLanguage: "Java",
+                        durationInMonths: 17,
+                        numberOfEmployees: 200000
+                    },
+                    {
+                        jobTitle: "Developer",
+                        company: "Siemens",
+                        mostUsedProgrammingLanguage: "C",
+                        durationInMonths: 24,
+                        numberOfEmployees: 300000
+                    }
+                ],
+                favoriteColor: "Red",
+                favoriteOperatingSystem: "Windows 10",
+                hobbies: [""],
+                keyAttributes: ["9-5 job", "Features, Features, Features"],
+                personalDrive: [""],
+                preferredCommunicationChannels: ["Slack", "IRC", "Twitter"],
+                programmingExperiences: [
+                    {
+                        language: "Java",
+                        experienceInYears: 4
+                    },
+                    {
+                        language: "C",
+                        experienceInYears: 7
+                    },
+                    {
+                        language: "PHP",
+                        experienceInYears: 3
+                    }
+                ],
+                usedTechnologies: [
+                    {
+                        name: "Git",
+                        experienceLevel: 3
+                    },
+                    {
+                        name: "Jenkins",
+                        experienceLevel: 2
+                    },
+                    {
+                        name: "Maven",
+                        experienceLevel: 4
+                    }
+                ]
             }
         ];
     }
--- src/components/personas/Persona.vue
+++ src/components/personas/Persona.vue
@@ -1,90 +1,90 @@
 <template>
     <div class="py-5">
-    <div class="container border p-2">
-      <div class="row justify-content-center text-center">
-        <div class="col-md-3">
-          <h1 class="text-info">{{`${persona.prename} ${persona.name}`}}</h1>
-        </div>
-        <div class="col-md-3">
-          <h3 class="" contenteditable="true"><i class="fas fa-hand-paper"></i> Junior Java Developer</h3>
-        </div>
-        <div class="col-md-3">
-          <h3 class=""><i class="fas fa-globe"></i> Germany</h3>
-        </div>
-        <div class="col-md-3">
-            
-          </div>
-      </div>
-      <div class="row">
-        <div class="col-md-4 justify-content-center">
-          <div class="card box-shadow">
-            <img class="card-img-top" src="assets/application-1934972_640.jpg">
-            <div class="card-body">
-              <p class="card-text"><i class="fas fa-heartbeat"></i> 32 years, married</p>
-              <p class="card-text"><i class="fas fa-map-marker"></i> Main Street 12, 75432 Neustadt</p>
-              <p class="card-text"><i class="fas fa-graduation-cap"></i> Education: Diplom Informatiker</p>
-              <p class="card-text"><i class="fas fa-quote-right"></i> “Without requirements or design, programming is the art of adding bugs to an empty text file.” - Louis Srygley</p>
-              
-                
-              
-              <div class="d-flex justify-content-between align-items-center">
-                <div class="btn-group">
-                  <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                  <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                </div> <small class="text-muted">made with personagenerator.net</small> </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-4">
-          <p class="lead my-2">Goals</p>
-          <ul class="">
-            <li>9-5 job</li>
-            <li>Features, Features, Features</li>
-          </ul>
+        <div class="container border p-2">
+            <div class="row justify-content-center text-center">
+                <div class="col-md-3">
+                    <h1 class="text-info">{{`${persona.prename} ${persona.name}`}}</h1>
+                </div>
+                <div class="col-md-3">
+                    <h3 class="" contenteditable="true">
+                        <i class="fas fa-hand-paper"></i> {{persona.currentJob.jobTitle}}</h3>
+                </div>
+                <div class="col-md-3">
+                    <h3 class="">
+                        <i class="fas fa-globe"></i> {{persona.country}}</h3>
+                </div>
+                <div class="col-md-3">
 
-          
-              
-              
-              <ul class="list-group">
-                <li class="list-group-item d-flex justify-content-between align-items-center"> C++<span class="badge badge-primary badge-pill">14</span> </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center"> Java<span class="badge badge-primary badge-pill">2</span> </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center"> JavaScript<span class="badge badge-primary badge-pill">1</span> </li>
-              </ul>
-              <p class="lead my-2">Technology Experience</p>
-              <ul class="list-group">
-                <li class="list-group-item d-flex justify-content-between align-items-center"><i class="fas fa-microchip"></i> Embedded<span class="badge badge-primary badge-pill">14</span> </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center"><i class="fab fa-windows"></i> Windows Server Administration<span class="badge badge-primary badge-pill">5</span> </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center"><i class="fas fa-sync"></i> Continuous Integration<span class="badge badge-primary badge-pill">3</span> </li>
-              </ul>
-            
-        </div>
-        <div class="col-md-4">
-          <div class="col-md-12">
-            <p class="lead m-2">Previous Jobs/Companies</p>
-            <ul class="list-group">
-              <li class="list-group-item d-flex justify-content-between align-items-center"> Test Engineer @ Microsoft<span class="badge badge-primary badge-pill">14</span> </li>
-              <li class="list-group-item d-flex justify-content-between align-items-center"> C++ Developer @ Google<span class="badge badge-primary badge-pill">2</span> </li>
-            </ul>
-            <p class="lead my-2">Communication Preferences</p>
-            <p>Slack, IRC, Twitter</p>
-            <p class="lead">Hobbies and Activities</p>
-            <div class="progress">
-              <div class="progress-bar" role="progressbar" style="width: 30%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
+                </div>
             </div>
-            <p class="">Outside &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Inside
-              <br> </p>
-            <p class="lead">Other
-              <br>
-            </p>
-            <p>Favorite Color: Red
-              <br>Disabilities: Glasses
-              <br>
-            </p>
-          </div>
+            <div class="row">
+                <div class="col-md-4 justify-content-center">
+                    <div class="card box-shadow">
+                        <img class="card-img-top" src="assets/application-1934972_640.jpg">
+                        <div class="card-body">
+                            <p class="card-text">
+                                <i class="fas fa-heartbeat"></i> {{persona.age}} years, married</p>
+                            <p class="card-text">
+                                <i class="fas fa-map-marker"></i> {{persona.street}} {{persona.number}}, {{persona.zipCode}} {{persona.city}}</p>
+                            <p class="card-text">
+                                <i class="fas fa-graduation-cap"></i> Education: {{persona.education}}</p>
+                            <p class="card-text">
+                                <i class="fas fa-quote-right"></i> {{persona.quote}}</p>
+
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">made with personagenerator.net</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <p class="lead my-2">Key Attributes and Goals</p>
+                    <ul class="">
+                        <li v-for="attr in persona.keyAttributes" v-bind:key="attr">{{attr}}</li>
+                    </ul>
+                    <p class="lead my-2">Programmin Languages</p>
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items-center" v-for="ex in persona.programmingExperiences" v-bind:key="ex.language"> {{ex.language}}
+                            <span class="badge badge-primary badge-pill">{{ex.experienceInYears}}</span>
+                        </li>
+                    </ul>
+                    <p class="lead my-2">Technology Experience</p>
+                    <ul class="list-group">
+                        <li class="list-group-item d-flex justify-content-between align-items-center" v-for="ex in persona.usedTechnologies" v-bind:key="ex.name">{{ex.name}}
+                            <span class="badge badge-primary badge-pill">{{ex.experienceLevel}}</span>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-4">
+                    <div class="col-md-12">
+                        <p class="lead m-2">Previous Jobs/Companies</p>
+                        <ul class="list-group">
+                            <li class="list-group-item d-flex justify-content-between align-items-center" v-for="job in persona.previousJobs" v-bind:key="job.jobTitle+job.company"> {{job.jobTitle}} @ {{job.company}}
+                                <span class="badge badge-primary badge-pill">{{Math.ceil(job.durationInMonths/12)}}</span>
+                            </li>
+                        </ul>
+                        <p class="lead my-2">Communication Preferences</p>
+                        <p>{{persona.preferredCommunicationChannels.join(", ")}}</p>
+                        <p class="lead">Hobbies and Activities</p>
+                        <div class="progress">
+                            <div class="progress-bar" role="progressbar" style="width: 30%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
+                        </div>
+                        <p class="">Outside &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Inside</p>
+                        <p class="lead">Other
+                            <br>
+                        </p>
+                        <p>Favorite Color: {{persona.favoriteColor}}
+                            <br>Favorite OS: {{persona.favoriteOperatingSystem}}
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
