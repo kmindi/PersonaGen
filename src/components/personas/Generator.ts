8fdabdb616f593776f40d7ecb93ae8c8207dbcee
Kai Mindermann <kai.mindermann@informatik.uni-stuttgart.de>
2018-04-04 23:11:19 +0200

    refactors Generator.generate()

--- src/components/personas/Generator.ts
+++ src/components/personas/Generator.ts
@@ -31,6 +31,15 @@ export class Generator {
     }
 
     public static generate(numberOfPersonas: number = 1): IPersona[] {
+        let personas = [];
+        for (let i = 0; i < numberOfPersonas; i++) {
+            personas.push(Generator.generateSingle());
+        }
+        console.log(personas);
+        return personas;
+    }
+
+    public static generateSingle(): IPersona {
 
         const place = Generator.getRandomObjectFromList(places);
         let portraitPrefix;
@@ -43,156 +52,82 @@ export class Generator {
         const portrait = Generator.getRandomObjectFromList(portraits[portraitPrefix]);
         const portraitFileName = portrait.fileName;
         const age = portrait.age;
-        
-        
+
+
         const operatingSystem = Generator.getRandomObjectFromList(operatingSystems);
 
-        return [
-            {
-                prename: "Otto",
-                name: "Mustermann",
-                country: "Germany",
-                age,
-                gender: "male",
-                maritalStatus: "married",
-                street: "Main Street",
-                streetNumber: "12",
-                zipCode: 75432,
-                city: place.name,
-                image: portraitPrefix + "/" + portraitFileName,
-                education: "Diplom Informatiker",
-                quote: "Without requirements or design, programming is the art of adding bugs to an empty text file. - Louis Srygley",
-                languages: ["German", "English"],
-                currentJob: {
-                    jobTitle: "Java Developer",
-                    company: "Red Hat",
-                    mostUsedProgrammingLanguage: "Java",
-                    numberOfEmployees: 10000
-                },
-                previousJobs: [
-                    {
-                        jobTitle: "Test Engineer",
-                        company: "Microsoft",
-                        mostUsedProgrammingLanguage: "Java",
-                        durationInMonths: 17,
-                        numberOfEmployees: 200000
-                    },
-                    {
-                        jobTitle: "Developer",
-                        company: "Siemens",
-                        mostUsedProgrammingLanguage: "C",
-                        durationInMonths: 24,
-                        numberOfEmployees: 300000
-                    }
-                ],
-                favoriteColor: "Red",
-                favoriteOperatingSystem: operatingSystem,
-                hobbies: ["Football", "Jogging", "Gym"],
-                keyAttributes: ["9-5 job", "Features, Features, Features"],
-                personalDrive: ["Clean Code", "Know your colleagues"],
-                preferredCommunicationChannels: ["Slack", "IRC", "Twitter"],
-                programmingExperiences: [
-                    {
-                        language: "Java",
-                        experienceInYears: 4
-                    },
-                    {
-                        language: "C",
-                        experienceInYears: 7
-                    },
-                    {
-                        language: "PHP",
-                        experienceInYears: 3
-                    }
-                ],
-                usedTechnologies: [
-                    {
-                        name: "Git",
-                        experienceLevel: 3
-                    },
-                    {
-                        name: "Jenkins",
-                        experienceLevel: 2
-                    },
-                    {
-                        name: "Maven",
-                        experienceLevel: 4
-                    }
-                ]
+        return {
+            prename: "Otto",
+            name: "Mustermann",
+            country: "Germany",
+            age,
+            gender: "male",
+            maritalStatus: "married",
+            street: "Main Street",
+            streetNumber: "12",
+            zipCode: 75432,
+            city: place.name,
+            image: portraitPrefix + "/" + portraitFileName,
+            education: "Diplom Informatiker",
+            quote: "Without requirements or design, programming is the art of adding bugs to an empty text file. - Louis Srygley",
+            languages: ["German", "English"],
+            currentJob: {
+                jobTitle: "Java Developer",
+                company: "Red Hat",
+                mostUsedProgrammingLanguage: "Java",
+                numberOfEmployees: 10000
             },
-            {
-                prename: "Erika",
-                name: "Mustermann",
-                country: "Germany",
-                age: 35,
-                gender: "female",
-                maritalStatus: "married",
-                street: "Main Street",
-                streetNumber: "12",
-                zipCode: 75432,
-                city: "Neustadt",
-                image: "",
-                education: "Diplom Informatiker",
-                quote: "Without requirements or design, programming is the art of adding bugs to an empty text file. - Louis Srygley",
-                languages: ["German", "English"],
-                currentJob: {
-                    jobTitle: "Java Developer",
-                    company: "Red Hat",
+            previousJobs: [
+                {
+                    jobTitle: "Test Engineer",
+                    company: "Microsoft",
                     mostUsedProgrammingLanguage: "Java",
+                    durationInMonths: 17,
                     numberOfEmployees: 200000
                 },
-                previousJobs: [
-                    {
-                        jobTitle: "Test Engineer",
-                        company: "Microsoft",
-                        mostUsedProgrammingLanguage: "Java",
-                        durationInMonths: 17,
-                        numberOfEmployees: 200000
-                    },
-                    {
-                        jobTitle: "Developer",
-                        company: "Siemens",
-                        mostUsedProgrammingLanguage: "C",
-                        durationInMonths: 24,
-                        numberOfEmployees: 300000
-                    }
-                ],
-                favoriteColor: "Red",
-                favoriteOperatingSystem: "Windows 10",
-                hobbies: ["Table Tennis", "Chess"],
-                keyAttributes: ["9-5 job", "Features, Features, Features"],
-                personalDrive: ["Clean Code", "Know your colleagues"],
-                preferredCommunicationChannels: ["Slack", "IRC", "Twitter"],
-                programmingExperiences: [
-                    {
-                        language: "Java",
-                        experienceInYears: 4
-                    },
-                    {
-                        language: "C",
-                        experienceInYears: 7
-                    },
-                    {
-                        language: "PHP",
-                        experienceInYears: 3
-                    }
-                ],
-                usedTechnologies: [
-                    {
-                        name: "Git",
-                        experienceLevel: 3
-                    },
-                    {
-                        name: "Jenkins",
-                        experienceLevel: 2
-                    },
-                    {
-                        name: "Maven",
-                        experienceLevel: 4
-                    }
-                ]
-            }
-        ];
+                {
+                    jobTitle: "Developer",
+                    company: "Siemens",
+                    mostUsedProgrammingLanguage: "C",
+                    durationInMonths: 24,
+                    numberOfEmployees: 300000
+                }
+            ],
+            favoriteColor: "Red",
+            favoriteOperatingSystem: operatingSystem,
+            hobbies: ["Football", "Jogging", "Gym"],
+            keyAttributes: ["9-5 job", "Features, Features, Features"],
+            personalDrive: ["Clean Code", "Know your colleagues"],
+            preferredCommunicationChannels: ["Slack", "IRC", "Twitter"],
+            programmingExperiences: [
+                {
+                    language: "Java",
+                    experienceInYears: 4
+                },
+                {
+                    language: "C",
+                    experienceInYears: 7
+                },
+                {
+                    language: "PHP",
+                    experienceInYears: 3
+                }
+            ],
+            usedTechnologies: [
+                {
+                    name: "Git",
+                    experienceLevel: 3
+                },
+                {
+                    name: "Jenkins",
+                    experienceLevel: 2
+                },
+                {
+                    name: "Maven",
+                    experienceLevel: 4
+                }
+            ]
+        };
     }
 
 }
