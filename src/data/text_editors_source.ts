1440d26f1aeff3f1ef5bcc9e56da5bea9798418e
Kai Mindermann <kai.mindi@gmx.de>
2018-04-11 19:43:51 +0200

    adds text_editors_source.ts

---
+++ src/data/text_editors_source.ts
@@ -0,0 +1,16 @@
+export const textEditors = [
+    "Atom",
+    "Brackets",
+    "Eclipse",
+    "Emacs",
+    "Gedit",
+    "Microsoft Visual Studio",
+    "NetBeans",
+    "Notepad++",
+    "SlickEdit",
+    "Sublime Text",
+    "TextMate",
+    "vi/Vim",
+    "Visual Studio Code",
+    "Xcode"
+]
